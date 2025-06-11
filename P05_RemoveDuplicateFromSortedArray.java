95917d8f07a1a69c5af308b286e2cb166ad5e46a
kshitij-parad <kshitijparad964@gmail.com>
2025-06-11 19:48:32 +0530

    Problem 05  Remove Duplicate From Sorted Array(2 pointer).

--- P05_RemoveDuplicateFromSortedArray.java
+++ P05_RemoveDuplicateFromSortedArray.java
@@ -12,7 +12,25 @@ public class P05_RemoveDuplicateFromSortedArray {
             inp[i] = sc.nextInt();
         }
 
-        
+        int j =0;
+
+        //1, 1, 1, 2, 2, 3
+
+        for(int i = 1 ; i< inp.length ; i++){
+            if(inp[i] != inp[j]){
+                j++;
+                inp[j] = inp[i];
+                
+            }
+        }
+
+        for(int k = j+1; k < inp.length; k++){
+            inp[k] = -1;
+        }
+
+     for (int i = 0; i < inp.length; i++) {
+         System.out.print(inp[i]+" ");
+     }
        
     }
 }
